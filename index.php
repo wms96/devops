2de19af188359b225927087a31fc64eec36acf35
ramy merheb <rmerheb@addbloom.com>
2021-05-16 10:05:48 +0300

    single file

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if($result = mysqli_query($conn, $sql)){
            // echo $row['phrase'];
     //$response = array();
     $response =  $row['phrase'];
-echo json_encode($response."asdsadasdas");
+echo json_encode($response);
 
 
     }
