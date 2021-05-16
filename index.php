cab009f38e506ef136d6825bdfcfaec97e21b5c3
ramy merheb <rmerheb@addbloom.com>
2021-05-16 15:58:59 +0300

    test

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if($result = mysqli_query($conn, $sql)){
            // echo $row['phrase'];
     //$response = array();
     $response =  $row['phrase'];
-echo json_encode($response."ramy22233");
+echo json_encode($response."ramy22233123213");
 
 
     }
